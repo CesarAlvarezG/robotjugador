da7ba64bde5a08bca2dd3db4cb67ab5b694b54e7
Cesar Alvarez <ing.cesaralvarezg@gmail.com>
2019-02-20 08:38:40 -0500

    Orden por el modulo bluetooth

--- robotjugador.ino
+++ robotjugador.ino
@@ -1,13 +1,17 @@
 #include<Servo.h>//Libreria para manejo de los servos
-
+#include<SoftwareSerial.h>
 #define PINSERVODERECHO 12//Defino el pin del servo derecho
+#define TXBluetooth 3 //Lugar de conexion d e l Blue tooth Rx
+#define RXBluetooth 2 //Lugar de conexion d e l Blue tooth Tx
+
 
 Servo motorDerecho;
-
+SoftwareSerial BT(RXBluetooth,TXBluetooth);
 
 void setup() {
   motorDerecho.attach(PINSERVODERECHO);
   Serial.begin(9600);
+  BT.begin(9600);
 }
 
 char orden;
@@ -17,6 +21,11 @@ void loop() {
     {
         orden=Serial.read();
     }
+  if(BT.available()>0)
+    {
+        orden=BT.read();
+        Serial.println(orden);  
+    }  
   if(orden=='w')
     {
       motorDerecho.write(0);//Sentido horario

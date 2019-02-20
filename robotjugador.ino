399753ddfe18a2dc601057a525aa862726d4ef3e
Cesar Alvarez <ing.cesaralvarezg@gmail.com>
2019-02-20 08:16:20 -0500

    Recibir orden del puerto usb

--- robotjugador.ino
+++ robotjugador.ino
@@ -7,16 +7,29 @@ Servo motorDerecho;
 
 void setup() {
   motorDerecho.attach(PINSERVODERECHO);
+  Serial.begin(9600);
 }
 
+char orden;
+
 void loop() {
-  motorDerecho.write(0);//Sentido horario
-  delay(1000);
-  motorDerecho.write(90);//Detenido
-  delay(1000);
-  motorDerecho.write(180);//Sentido antihorario
-  delay(1000);
-  motorDerecho.write(90);//Detenido
-  delay(1000);
+  if(Serial.available()>0)
+    {
+        orden=Serial.read();
+    }
+  if(orden=='w')
+    {
+      motorDerecho.write(0);//Sentido horario
+      delay(1000);
+      motorDerecho.write(90);//Detenido
+      delay(1000);
+      motorDerecho.write(180);//Sentido antihorario
+      delay(1000);
+      motorDerecho.write(90);//Detenido
+      delay(1000);    
+    }else{
+          motorDerecho.write(90);//Detenido
+         }  
+  
   
 }
